9cc8ebfa466994fcbcf80587f02e31daf41c4703
angelo4ka <karina3125@yandex.ru>
2022-11-30 01:01:00 +0300

    Add project files.

---
+++ VelvetBrowsApp/Pages/PageAddEditService.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BeautyShopApp.Pages
+{
+    /// <summary>
+    /// Interaction logic for PageAddEditService.xaml
+    /// </summary>
+    public partial class PageAddEditService : Page
+    {
+        public PageAddEditService()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
